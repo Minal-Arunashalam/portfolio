8273460b3715572e64bc78662a5cb9d5080a804b
Minal-Arunashalam <minal.shalam@gmail.com>
2024-02-09 15:26:20 -0500

    finished light/dark theme, and switching of button icons between themes

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ function toggleTheme() {
     document.body.classList.toggle('dark-mode');
 }
 
+function toggleIcon(){
+    var sun = document.getElementById('sun');
+    var moon = document.getElementById('moon');
+    
+    // sun.toggle('hidden');
+    // moon.toggle('moon-icon');
+    sun.classList.toggle('hidden');
+    moon.classList.toggle('hidden');
+}
+
 // action for theme toggle button
 document.getElementById('theme-toggle').addEventListener('click', function() {
     toggleTheme();
+    toggleIcon();
 });
